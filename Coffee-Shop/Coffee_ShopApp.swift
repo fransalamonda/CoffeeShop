f4fb430a511cac21eedda9404452cc92e459cf99
franssalamonda <mondlightmoon@gmail.com>
2022-11-22 17:03:36 +0700

    Initial Commit

---
+++ Coffee-Shop/Coffee_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_ShopApp.swift
+//  Coffee-Shop
+//
+//  Created by Frans Sala Monda on 22/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
